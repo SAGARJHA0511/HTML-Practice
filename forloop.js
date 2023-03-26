8f8684498ff13751d64ee317a815426a6e8091b4
Sagar Jha <sagarjha0511@gmail.com>
2023-03-26 22:05:29 +0530

    ok

--- forloop.js
+++ forloop.js
@@ -1,9 +1,20 @@
 
-var zee = [5, 55, 555, 5555, 55555, 555555];  
+/*var zee = [5, 55, 555, 5555, 55555, 555555];  
 let i = 1;  
 for(var z in zee) {    
    console.log("values[" +i+ "]: " +zee[z]);  
     i++;  
 }  
 
-console.log('zee');
+console.log('zee');*/
+ 
+x=10;   
+var x;  
+console.log(x);
+
+var z=(sum(10,20));  
+function sum(a,b)  
+{  
+return a+b;  
+}  
+console.log(z)
