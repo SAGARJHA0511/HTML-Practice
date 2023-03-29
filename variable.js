b583990ea5df835ad03141db4c314c6b7ae08653
Sagar Jha <sagarjha0511@gmail.com>
2023-03-29 19:15:08 +0530

    ok

--- variable.js
+++ variable.js
@@ -1,4 +1,15 @@
-var firstName = "oggy";
-console.log(firstName);
-firstName = "mohit";
-console.log(firstName)
+// var firstName = "oggy";
+// console.log(firstName);
+// firstName = "mohit";
+//console.log(firstName)
+let myArr=[]
+
+let i=5
+while(i>0)
+{
+    
+    myArr.pop(i)
+    
+    i--;
+}
+
