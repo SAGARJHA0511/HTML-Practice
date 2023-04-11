c5e74ce5071a9bff56aec4712575d6cb75ada263
Sagar Jha <sagarjha0511@gmail.com>
2023-04-11 13:03:23 +0530

    windiow

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-var x=1;
+/*var x=1;
 a();
 b();
 console.log(x);
@@ -11,5 +11,15 @@ function a() {
 function b(){
     var x=100;
     console.log(x);
-    
+
+}*/
+
+var a =10;
+function b(){
+    var x=10;
+
 }
+//console.log(window.a);
+console.log(a);
+console.log(x);
+
