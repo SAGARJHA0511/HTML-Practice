5db231ea63aedeaaba89999728713de0c2f94b41
Sagar Jha <sagarjha0511@gmail.com>
2023-04-17 17:44:07 +0530

    ok

--- function.js
+++ function.js
@@ -24,4 +24,18 @@ console.log(a);
 console.log(x);*/
 
 
+function x(){
+    for (var i=1; i<=5; i++){
+        function close(i){
+            setTimeout (function(){
+                console.log(i);
+
+            }, i*1000);
+       }
+       close(i);
+
+    }
+    console.log("Hello Oggy");
+}
+x();
 
