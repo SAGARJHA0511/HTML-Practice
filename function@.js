f16f1eef439249500bedf78fb8872ea1b49913a0
Sagar Jha <sagarjha0511@gmail.com>
2023-04-19 01:31:41 +0530

    ok

---
+++ function@.js
@@ -0,0 +1,30 @@
+// function statement 
+
+function a(){
+    console.log("a called ");
+
+}
+a();
+ 
+// function expression         
+ 
+var b= function(){
+    console.log("b called");
+}
+b();
+
+// function anonymous 
+
+function/* used as value */ (){
+
+}
+
+// name function experssion
+ 
+var b = function xyz(){
+    console.log("b called");
+
+}
+b()
+
+//
