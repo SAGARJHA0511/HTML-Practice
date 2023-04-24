d39289e4dd1e147d4c736d9158db3bbf1c705fe3
Sagar Jha <sagarjha0511@gmail.com>
2023-04-24 17:19:10 +0530

    ok

--- function.js
+++ function.js
@@ -76,7 +76,7 @@ function second(){
 }
 
  first();
- second();*/
+ second();
 
  console.log("Start");
 
@@ -84,9 +84,26 @@ function second(){
     console.log("callback");
 
 
- } , 5000)
+ } , 5000)*/
 
- 
+ console.log("Start");
+
+ setTimeout(function cb(){
+    console.log("Call back");
+
+ }, 5000);
+
+ console.log("End");
+
+ let startDate = new Date().getTime();
+ let endDate = startDate;
+ while (endDate < startDate + 10000) {
+    endDate = new Date().getTime();
+
+ }
+    
+   console.log("While Expires");
+
 
 
 
