c2382720b882d7215e086e63df99336b3665d7c4
Sagar Jha <sagarjha0511@gmail.com>
2023-04-25 08:15:44 +0530

    ok

--- function@.js
+++ function@.js
@@ -25,7 +25,7 @@ var b = function xyz(){
     console.log("b called");
 
 }
-b()*/
+b()
 
 // binary
 
@@ -33,7 +33,33 @@ const arr=[5,3,2,6];
 
 const output = arr.map((x)=> x.toString(2));
 
+console.log(output);*/
+
+// Map function 
+
+const arr =[5,3,2,6];
+
+//double 
+
+function double (x){
+ return  x*2;
+
+}
+
+ // triple 
+
+ function triple(x){
+     return x*3;
+     
+    }
+ 
+ 
+  
+const output = arr.map(triple);
+
 console.log(output);
+
+
 
 
 
