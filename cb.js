a7ddc5d8116d740417a09906083871faca30ed3e
Sagar Jha <sagarjha0511@gmail.com>
2023-04-29 11:32:06 +0530

    ok

--- cb.js
+++ cb.js
@@ -1,4 +1,4 @@
-console.log("Hello");
+/*console.log("Hello");
 
 setTimeout(function(){
     console.log("JavaScript");
@@ -6,3 +6,34 @@ setTimeout(function(){
 },5000);
 
 console.log("Oggy");
+
+function myfirst(){
+    mydisplay("Hello");
+    console.log(myfirst);
+
+}
+
+function mysecond(){
+    mydisplay("Goodbey");
+    console.log(mysecond);
+}
+
+myfirst();
+mysecond();
+
+function myFirst() {
+    myDisplayer("Hello");
+  }
+  
+  function mySecond() {
+    myDisplayer("Goodbye");
+  }
+  
+  myFirst();
+  mySecond();*/
+
+  let arr =[1,2,3,4,5];
+  let obj ={...arr};
+
+  console.log(obj);
+
