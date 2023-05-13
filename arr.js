a1259597755c4904169cef112ca5f7596ab746a5
Sagar Jha <sagarjha0511@gmail.com>
2023-05-13 21:51:02 +0530

    ok

--- arr.js
+++ arr.js
@@ -1,10 +1,29 @@
 // Remove duplicates 
 
-let arr = [1,1,13,5,5,,5,4,4,9];
+/*let arr = [1,1,13,5,5,,5,4,4,9];
 
-function removeDuplicates(arr) {
+function rD(arr) {
 	return arr.filter((item,
 		index) => arr.indexOf(item) === index);
 }
 
-console.log(removeDuplicates(arr));
+console.log(rD(arr));
+
+
+
+var A = [ 1, 2, 3, 4, 5 ];
+console.log( A.flatMap(x => [x * 3]))
+
+b = A.reduce((acc, x) => acc.concat([ x * 3 ]), []);
+console.log(b);*/
+
+
+var set1 = new Set(["sumit","sumit","amit","oggy","anish", "oggy"]);
+
+var set2 = new Set("fooooooood");
+
+var set3 = new Set([10, 20, 30, 30, 40, 40]);
+
+var set4 = new Set("goooooon");
+console.log(set4);
+
